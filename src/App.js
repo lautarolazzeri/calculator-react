91bcbb268cc7cdc881f99421ea7b1ef95de2ef25
Lautaro <lautyylazzerii@gmail.com>
2022-02-07 13:23:42 -0300

    Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {CalculatorApp} from './CalculatorApp'
+import './app.css'
 
-export default App;
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import calculatorAppReducers from './reducers/'
+
+const store = createStore(calculatorAppReducers)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <CalculatorApp />
+    </Provider>,
+    document.getElementById('app')
+);
